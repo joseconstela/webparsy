10d5103ab99df6d707f386dd8013a3a01ce0d96e
Jose Constela <jose@joseconstela.com>
2019-04-22 12:28:48 +0200

    Support GET method

--- helpers/methods/goto.js
+++ helpers/methods/goto.js
@@ -1,8 +1,18 @@
 const schema = {
   method: 'goto',
   process: (flags, page, params, html) => {
-    const url = params.flag ? flags[params.flag] : params
-    return page.goto(url)
+
+    // URL can be passed as:
+    // - goto: example.com
+    // or
+    // - goto:
+    //     flag: websiteUrl
+    // or
+    // - goto:
+    //     url: example.com
+    
+    const url = params.flag ? flags[params.flag] : params.url ? params.url : params
+    return params.method === 'get' ? url : page.goto(url)
   }
 }
 
