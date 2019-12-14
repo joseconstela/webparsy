1556f99b7db26a7f57e9bc8eda3c8e1050ba54bc
Jose Constela <jose@joseconstela.com>
2019-12-14 12:40:09 +0100

    Remove meow

--- cli.js
+++ cli.js
@@ -1,17 +1,26 @@
 #!/usr/bin/env node
 'use strict'
 
-const meow = require('meow')
- 
-const cli = meow(`
-    Usage
-      $ webparsy <yaml file>
-`, {
-    
+const printHelp = () => console.log(`Usage
+  $ webparsy <yaml file>`)
+
+let node, webparsy, file, flags = {}
+node = process.argv[0]
+webparsy = process.argv[1]
+file = process.argv[2]
+
+if (!file) return printHelp()
+
+process.argv.slice(3).map((a, i) => {
+  let next = process.argv[3+i+1]
+  if (a.startsWith('--')) {
+    if (next.startsWith('--')) throw new Error('Invalid argument')
+    flags[a.replace('--', '')] = next
+  }
 })
 
 require('.').init({
-  file: cli.input[0],
-  flags: cli.flags,
+  file,
+  flags,
   cli: true
 })
