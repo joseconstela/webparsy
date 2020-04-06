ca45038e6e275063401a13323746c6d5926563ed
Jose Constela <jose@joseconstela.com>
2020-04-06 19:50:22 +0200

    Add click xpath

--- helpers/methods/click.js
+++ helpers/methods/click.js
@@ -1,6 +1,23 @@
 const schema = {
   method: 'click',
-  puppeteer: true
+  process: async (flags, page, params, html, usingPuppeteer) => {
+    if (params.selector) {
+      await page.click(params.selector)
+    }
+    else if (params.xPath) {
+      const [button] = await page.$x(params.xPath)
+      if (button) {
+          await button.click()
+      }
+      await page.click(params.selector)
+    }
+    else if (params) {
+      await page.click(params)
+    }
+    else {
+      throw new Error('incorrect-click-options')
+    }
+  }
 }
 
 module.exports = schema
